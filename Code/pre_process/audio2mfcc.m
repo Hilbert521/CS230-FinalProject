760cf8ca3b5746fc0050e61bc8bbb3869db0bba3
Lloyd Maza <lloydpmaza@gmail.com>
2018-03-05 12:35:44 -0800

    Pre-Processing Pipeline Completed
    Restructured pre-processing pipeline. Introduced a data storage folder.

--- Code/pre_process/audio2mfcc.m
+++ Code/pre_process/audio2mfcc.m
@@ -1,53 +1,141 @@
-% Script for converting .mp3 files to X and Y matrices
-% NOTE: I had to make some changes to accoutn for my old version of matlab
-
+% Script for converting raw .mp3 files to pre-processed MFCC data
 clear
 close all
+clc
 
-% preprocessing hyperparams
-window_size_ms = 10; %window length
-window_per_frame = 10; %windows in each frame (training example)
+% User inputs:
+% Flags
+writeVoicedData = true;     % Write the VAD-processed speech to .wav files
+writeAllMFCCData = true;    % Write all MFCC data for each audio file to .csv
+writeShuffledSets = true;   % Write the train, dev, and test sets to .csv
+
+% Pre-processing hyperparameters
+frame_size_ms = 10; %window length
+frames_per_window = 10; %windows in each frame (training example)
 frame_stride = 3; %stride of each frame in number of windows
 num_mfcc = 13; %desired number of mfcc coefficients 
 
-% Read through the contents of the audio folder
-audioFolder = 'Audio_Data';
-folderConts = dir(audioFolder);
+% Set the shuffling seed (if desired)
+rng(123);
+% rng('shuffle');
 
-for ii = 1:length(folderConts)
+% Set the desired breakdown of train, dev, and test data
+trainSize = 0.95;
+devSize = 0.025;
+testSize = 0.025;
+
+% Paths to raw and pre-processed data
+    % Note: based on git folder structure
+currentFile = mfilename('fullpath');
+[currentPath, ~, ~] = fileparts(currentFile);
+
+dataFolder = fullfile(currentPath, '..', '..', 'Data');
+rawAudioFolder = fullfile(dataFolder, 'Raw_Audio_Data');
+speechAudioFolder = fullfile(dataFolder, 'VAD-Processed_Audio_Data');
+mfccFolder = fullfile(dataFolder, 'MFCC_Data');
+
+% Read through the contents of the raw audio folder
+folderConts = dir(rawAudioFolder);
+
+% Initialize empty arrays for the full set of X and Y data
+fullX = [];
+fullY = [];
+
+for ii = 1:length(folderConts) 
     audioFile = folderConts(ii);
-    fprintf('File #%i of %i, %s \n', ii, length(folderConts), audioFile.name);
-    
-    [~, name, ext] = fileparts(audioFile.name);
-    class = 0;
-    
-    if isequal(ext, '.mp3')
-        [sig, Fs] = data_load_trim(fullfile(audioFolder,audioFile.name));
-        try
-            X = kannumfcc(13, sig, Fs, window_size_ms);
-            
-            X = data_frame(X, frame_stride, window_per_frame);
+    fprintf('File #%i of %i: %s \n', ii, length(folderConts), audioFile.name);
 
-            if ~isempty(strfind(name, 'obama'))
-                class = 1;
-            elseif ~isempty(strfind(name, 'trump'))
-                class = 2;
-            end
+    [~, fileName, ext] = fileparts(audioFile.name);
+    
+    % Only process mp3 files
+    if isequal(ext, '.mp3')
+        
+        % Pull the class from the file name
+        classStr = strtok(fileName, '-');
+        class = str2double(classStr);
+        
+        % Process only the voiced data from the raw audio
+        [voicedSig, Fs] = data_load_trim(fullfile(rawAudioFolder, audioFile.name));
+        
+        % Save the voiced data for reference
+        if writeVoicedData
+            % Note: audiowrite can't handle .mp3, defaulting to .wav
+            voicedDataPath = fullfile(speechAudioFolder, [fileName, '-Speech', '.wav']);
+            audiowrite(voicedDataPath, voicedSig, Fs);
+        end
+        
+        try
+            % Extract MFCCs
+            X = kannumfcc(13, voicedSig, Fs, frame_size_ms);
             
+            % Re-structure data to account for overlapping frames
+            X = data_frame(X, frame_stride, frames_per_window);
+            
+            % Generate the corresponding Y matrix
             Y = zeros(size(X, 1), 2);
             Y(:, class) = 1;
             
-            
-            % split data into overlapping windows
-            
             % Write X and Y to csv
-            Xfile = fullfile('MFCC_Data', [name, '_X.csv']);
-            Yfile = fullfile('MFCC_Data', [name, '_Y.csv']);
+            if writeAllMFCCData
+                Xfile = fullfile(mfccFolder, [fileName, '_X.csv']);
+                Yfile = fullfile(mfccFolder, [fileName, '_Y.csv']);
             
-            csvwrite(Xfile, X);
-            csvwrite(Yfile, Y);
+                csvwrite(Xfile, X);
+                csvwrite(Yfile, Y);
+            end
+            
+            % Expand the full X and Y sets
+            fullX = [fullX; X];
+            fullY = [fullY; Y];
         catch
-            fprintf('Skipped file #%i \n', ii);
+            fprintf('------------\n');
+            fprintf('Skipped file\n');
+            fprintf('------------\n');
         end
     end
 end
+
+if writeShuffledSets
+    fprintf('Writing full data sets\n');
+    % Shuffle the full X and Y matrices
+    m_full = size(fullX, 1);
+    shuffledIdx = randperm(m_full);
+    
+    shuffledX = fullX(shuffledIdx, :);
+    shuffledY = fullY(shuffledIdx, :);
+    
+    % Parse train, dev and test sets
+    m_train = floor(trainSize*m_full);
+    m_dev = floor(devSize*m_full);
+    m_test = floor(testSize*m_full);
+    
+    X_train = shuffledX(1:m_train, :);
+    Y_train = shuffledY(1:m_train, :);
+    
+    trainFileX = fullfile(mfccFolder, 'X_train.csv');
+    trainFileY = fullfile(mfccFolder, 'Y_train.csv');
+    
+    X_dev = shuffledX((m_train + 1):(m_train + m_dev), :);
+    Y_dev = shuffledY((m_train + 1):(m_train + m_dev), :);
+    
+    devFileX = fullfile(mfccFolder, 'X_dev.csv');
+    devFileY = fullfile(mfccFolder, 'Y_dev.csv');
+    
+    X_test = shuffledX((m_train + m_dev + 1):(m_train + m_dev + m_test), :);
+    Y_test = shuffledY((m_train + m_dev + 1):(m_train + m_dev + m_test), :);
+    
+    testFileX = fullfile(mfccFolder, 'X_test.csv');
+    testFileY = fullfile(mfccFolder, 'Y_test.csv');
+    
+    % Write out the train, dev, and test sets
+    csvwrite(trainFileX, X_train);
+    csvwrite(trainFileY, Y_train);
+    
+    csvwrite(devFileX, X_dev);
+    csvwrite(devFileY, Y_dev);
+    
+    csvwrite(testFileX, X_test);
+    csvwrite(testFileY, Y_test);
+end
+
+fprintf('Complete!\n');
--- Code/pre_process/data_frame.m
+++ Code/pre_process/data_frame.m
@@ -1,4 +1,4 @@
-function X_framed = data_frame(X, stride, win_per_frame)
+function X_framed = data_frame(X, stride, framesPerWindow)
 
 % splits data into windows according to the Ge paper
 
@@ -10,18 +10,19 @@ function X_framed = data_frame(X, stride, win_per_frame)
 % output:
 % X_framed, mfcc's of audio data split into frames 
 
-num_win = length(X(:,1));
+numFrames = size(X, 1);
+numFeatures = size(X, 2);
 
-m = floor((num_win-win_per_frame)/stride);
-X_framed = zeros(m, length(X(1,:))*win_per_frame);
+m = floor((numFrames - framesPerWindow)/stride);
+X_framed = zeros(m, numFeatures*framesPerWindow);
 
 for ii = 1:m
     
-    ind_start = (ii-1)*stride+1;
-    ind_end = ind_start+win_per_frame-1;
+    ind_start = (ii - 1)*stride + 1;
+    ind_end = ind_start + framesPerWindow - 1;
     
     sect = X(ind_start:ind_end, :);
-    X_framed(ii,:) = reshape(sect',1,[]);
+    X_framed(ii, :) = reshape(sect', 1, []);
     
 end
 
